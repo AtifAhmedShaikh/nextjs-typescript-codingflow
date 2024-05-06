6851ed4faa50277df051f19abf5ad8a1de88609a
AtifAhmadShaikh <atifahmad2219@gmail.com>
2024-05-07 00:39:23 +0500

    Add dynamic metatadata

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+export function generateMetadata({ params }: any) {
+  return {
+    title: `UsersById ${params.id}}`,
+  };
+}
+
 const UserById = ({ params }: { params: any }) => {
   return <div>UserById {params.id} </div>;
 };
