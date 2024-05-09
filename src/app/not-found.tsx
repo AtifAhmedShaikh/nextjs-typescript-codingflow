97b8d9c8356e89477bad4219c3f0f8b7f777291c
AtifAhmadShaikh <atifahmad2219@gmail.com>
2024-05-09 17:40:06 +0500

    Add styling using tailwindcss in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return to Home</Link>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h2 className="text-3xl font-semibold mb-4">Not Found</h2>
+      <p className="text-gray-600 mb-8">Could not find requested resource</p>
+      <Link href="/">
+        <a className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Return to Home
+        </a>
+      </Link>
     </div>
   );
 }
