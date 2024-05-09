8b90ae41d474f03abd923c8d0885dca78e805e88
AtifAhmadShaikh <atifahmad2219@gmail.com>
2024-05-09 18:25:37 +0500

    Add user model and DB connection

---
+++ src/models/UserModel.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const userModel = mongoose.model<User>("User", UserSchema);
+
+export default userModel;
