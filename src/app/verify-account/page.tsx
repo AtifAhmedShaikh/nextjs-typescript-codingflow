2068cdbd3324fceafb09704a69ccd33350d5cde9
AtifAhmadShaikh <atifahmad2219@gmail.com>
2024-05-13 23:35:37 +0500

    Add verify acount page

---
+++ src/app/verify-account/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const VerifyAccount = () => {
+    return (
+        <div>
+            <h2>
+                Verify Your Account
+            </h2>
+            <button>click to verify</button>
+        </div>
+    )
+}
+
+export default VerifyAccount
