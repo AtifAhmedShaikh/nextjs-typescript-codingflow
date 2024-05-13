e3bb72ebb295698a99dac35618c0b2562ecf4cf7
AtifAhmadShaikh <atifahmad2219@gmail.com>
2024-05-13 23:38:20 +0500

    Add styling in verify account page

--- src/app/verify-account/page.tsx
+++ src/app/verify-account/page.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const VerifyAccount = () => {
     return (
-        <div>
-            <h2>
+        <div className='px-auto m-auto text-center h-screen flex justify-center items-center flex-col gap-10'>
+            <h2 className='text-3xl text-center'>
                 Verify Your Account
             </h2>
-            <button>click to verify</button>
+            <button className='bg-emerald-600 hover:bg-emerald-400 rounded-md border w-fit px-3 py-1 text-gray-100 text-lg'>click to verify</button>
         </div>
     )
 }
